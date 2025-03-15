2a8b6fc4500ce35de8960b8580d6e2afcea92f5f
jrchyang <yujrchyang@gmail.com>
2025-03-15 13:46:53 +0800

    update getopt example

--- shell/example_getopt.sh
+++ shell/example_getopt.sh
@@ -2,7 +2,7 @@
 
 module="all"
 
-if ! GETOPT_ARGS=$(getopt -q -o rm:i --long rpm,module:inactive -- "$@");then
+if ! GETOPT_ARGS=$(getopt -q -o r:m: --long rpm:module: -- "$@");then
     echo "Error: Invalid option." >&2
     exit 1
 fi
@@ -12,21 +12,21 @@ eval set -- "$GETOPT_ARGS"
 while [ -n "$1" ]; do
     case "$1" in
         -r|--rpm)
-            echo "will build rpm"
-            shift
+            if [ -z "$2" ]; then
+                echo "Error: -m requires a value." >&2
+                exit 1
+            fi
+            echo "will build rpm $2"
+            shift 2
             ;;
         -m|--module)
             if [ -z "$2" ]; then
                 echo "Error: -m requires a value." >&2
                 exit 1
             fi
-	    module="$2"
+            module="$2"
             echo "will build module $module"
             shift 2
-            ;;
-        -i|--inactive)
-            echo "will create inactively"
-            shift
             ;;
         --)
             shift
